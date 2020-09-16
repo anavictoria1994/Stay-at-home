b4f3e8cf102714aa1a072630732892f637309c8f
baiest <ballesteros.juan@correounivalle.edu.co>
2020-09-16 12:45:36 -0500

    mdbvue agregado

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/css/mdb.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
+import * as mdbvue from 'mdbvue'
+for (const component in mdbvue) {
+    Vue.component(component, mdbvue[component])
+}
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
