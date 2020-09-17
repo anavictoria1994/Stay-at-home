67709be68eb53244db7863f740c674e73a2ac8c2
Febe Gaviria <febesita@MacBook-Pro-de-Febe.local>
2020-09-16 21:50:45 -0500

    new login

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
   <div id="app">
-    <HelloWorld/>
+   <Login/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from "./components/Login.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Login
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body{background: black}
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <div class="contenedor">
+        <form>
+            <p class="h4 text-center mb-4">Iniciar Sesión</p>
+             <div class="grey-text">
+                <mdb-input label="Your email" icon="envelope" type="email"/>
+                <mdb-input label="Your password" icon="lock" type="password"/>
+            </div>
+            <div class="text-center">
+                <mdb-btn>Login</mdb-btn>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.contenedor{
+    background: wheat;
+    margin-left: 29%;
+    margin-right: 30%;
+    margin-top: 15%;
+    padding: 3% ;
+    }
+    
+</style>
