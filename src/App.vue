2d47891b84e1132cec229bb50dd18542ee514f6d
Febe Gaviria <febesita@MacBook-Pro-de-Febe.local>
2020-09-17 19:03:45 -0500

    login modified

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,5 @@ export default {
 </script>
 
 <style>
-  body{background: black}
+  
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,16 @@
 
 <template>
     <div class="contenedor">
-        <form>
+        <form class="login">
             <p class="h4 text-center mb-4">Iniciar Sesión</p>
              <div class="grey-text">
                 <mdb-input label="Your email" icon="envelope" type="email"/>
                 <mdb-input label="Your password" icon="lock" type="password"/>
             </div>
+                <center href="#">
+                    <i class="fa fa-key"></i>
+                    Forgot your password?
+                    </center>
             <div class="text-center">
                 <mdb-btn>Login</mdb-btn>
             </div>
@@ -22,11 +26,25 @@ export default {
 
 <style scoped>
 .contenedor{
-    background: wheat;
-    margin-left: 29%;
-    margin-right: 30%;
-    margin-top: 15%;
-    padding: 3% ;
+    display: flex;
+    align-items: center;
+    flex-direction: column; 
+    justify-content: center;
+    width: 100%;
+    min-height: 100%;
+    padding: 20px;
+    background: #21B295;
     }
-    
+.login{
+    border-radius: 2px 2px 5px 5px;
+  padding: 10px 20px 20px 20px;
+  width: 90%;
+  max-width: 320px;
+  background: #ffffff;
+  position:relative;
+  top: 120px;
+  padding-bottom: 80px;
+  box-shadow: 0px 1px 5px
+}
+
 </style>
