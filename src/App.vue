879cb556f747a26b561a42261dc83eaf1e79183a
Juan Ballesteros <43350432+baiest@users.noreply.github.com>
2020-10-01 13:09:16 -0500

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,8 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 import Register from "./components/Login.vue"
-=======
 import Login from "./components/Login.vue"
->>>>>>> f1beee45918fde4d9a601bf8205c5886e58a3369
 
 export default {
   name: 'App',
--- src/main.js
+++ src/main.js
@@ -1,19 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbvue/lib/css/mdb.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueResource)
+Vue.use(VueRouter)
 
 import * as mdbvue from 'mdbvue'
 for (const component in mdbvue) {
     Vue.component(component, mdbvue[component])
 }
-Vue.config.productionTip = false
 
 new Vue({
     render: h => h(App),
