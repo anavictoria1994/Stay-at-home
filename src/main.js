0c5bb5f362841ac3e514770c299125f0870707fd
Juan Ballesteros <43350432+baiest@users.noreply.github.com>
2020-10-03 12:36:06 -0500

    mdv desintalado

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap-css-only/css/bootstrap.min.css'
-import 'mdbvue/lib/css/mdb.min.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -15,10 +13,6 @@ Vue.use(IconsPlugin)
 Vue.use(VueResource)
 Vue.use(VueRouter)
 
-import * as mdbvue from 'mdbvue'
-for (const component in mdbvue) {
-    Vue.component(component, mdbvue[component])
-}
 //MODULOS
 import login from './components/Login.vue'
 import register from './components/Register.vue'
