45624bbaead0d73e9ed512c17b6a9040e430a59c
Febe Gaviria <febesita@MacBook-Pro-de-Febe.local>
2020-10-10 23:22:54 -0500

    new login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,31 @@
 <template>
-  <b-container class="bv-example-row" id="contenedor1">
+  <b-container class="login-view" id="contenedor1">
     <b-row class="justify-content-md-center" id="principal">
         <b-col col lg="2"></b-col>
         <b-col cols="12" md="auto">
             <div class="panel-body">
                 <b-card img-alt="Card image" img-top>
-                    <img src="../images/manos.jpg">
-                    <b-card-text>
-                        <b-form-group>
-                            <div class="col">
-                                <label></label>
-                                <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
-                            </div>
-                            <div class="col">
-                                <b-form-input id="input-1" v-model="form.email" type="email" required placeholder="Enter email"></b-form-input>
-                            </div>
-                        </b-form-group>
-                    </b-card-text>
-                </b-card>
+                        <img src="../images/manos.jpg">
+                    <b-card>
+                        <b-card-text>
+                                <form @submit="onSubmit">
+                                    <div class="form-row mb-4">
+                                        <div class="col">
+                                            <b-form-input id="input-1" v-model="form.email" type="email" required placeholder="Correo"></b-form-input>
+                                        </div>
+                                    </div>
+                                    <div class="form-row mb-4">
+                                        <div class="col">
+                                            <b-form-input type="password" id="imputPass" v-model="form.pass" required placeholder="Contraseña"></b-form-input>
+                                        </div>
+                                    </div>
+                                    <div class="text-center">
+                                        <b-button variant="info" type="submit">Iniciar Sesión</b-button>
+                                    </div>
+                                </form>
+                        </b-card-text>
+                    </b-card>
+                </b-card>   
             </div>    
         </b-col>
         <b-col col lg="2"></b-col>
@@ -50,7 +58,7 @@ export default {
 
 body{
     padding-top: 5%;
-    min-width: 364px;
+    min-width: 438px;
     background-color: #C7FFF5;
     padding-bottom: 5%;
 }
